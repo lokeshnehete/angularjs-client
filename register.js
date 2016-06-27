5d07b06bdba0ec0572b9e6f31f4e8ed4136f27e7
iamukt <iamukt@gmail.com>
2016-06-27 10:52:19 +0530

    Front End initiated

---
+++ register.js
@@ -0,0 +1,23 @@
+var myApp = angular.module('module', []);
+myApp.controller('registerTraining', function($scope,$http)
+{
+	this.postData = function() 
+	{
+		console.log(this.inputData.startDate);
+		console.log(this.inputData.name);
+		var encodedString = 'name='+
+		encodeURIComponent(this.inputData.name) +
+		'&startDate='+
+		encodeURIComponent(this.inputData.startDate);
+		$http(
+				{   method:'POST',
+					url:"http://localhost:9090/springmvchibernate/registerTraining",
+					data: encodedString,
+					headers: {'Content-Type' : 'application/x-www-form-urlencoded'}
+				}
+			).success(function(data,status,headers,config) 
+				{
+		 	/*console.log(data);*/
+		 })		
+	}	
+})
