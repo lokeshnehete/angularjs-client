d3ff1fa7c8d87413951498bd2c8e73375af98f0e
iamukt <iamukt@gmail.com>
2016-06-28 21:00:12 +0530

    Added Bootstrap and Routing also updated

--- controller.js
+++ controller.js
@@ -14,16 +14,20 @@
     .when('/dashboard',{
         templateUrl: 'dashboard.html'
     })
+    .when('/registertraining',{
+        templateUrl: 'registertraining.html'
+    })
     .otherwise({
  		redirectTo:'/'
  	});
  });
 
- myApp.controller('loginController', function($scope,$http,$location)
+ myApp.controller('loginController', function($rootScope,$scope,$http,$location)
  {
  	$scope.authenticateUser = function () {
- 		var User = { username: $scope.tm.login.username, password: $scope.tm.login.password};
- 		$http(
+        var User = { username: $scope.tm.login.username, password: $scope.tm.login.password};
+        console.log(User);
+        $http(
 				{   method:'POST',
 					url:"http://localhost:8080/springmvchibernate/login",
 					data: User,
@@ -31,8 +35,8 @@
 				}
 			).success(function(data,status,headers,config) 
 				{
-            console.log("Success");
-            console.log("EmpType: "+data.empType);
+            $rootScope.role = data.empType;
+            console.log('role in http'+$rootScope.role);
 		 	if(data.empType=='TrainingExecutive')
                 $location.path("/admindashboard");
             else if(data.empType=='Trainer')
@@ -40,12 +44,64 @@
             else if(data.empType=='Employee')
                 $location.path('/dashboard');
             else 
-                $location.path('/')
+            {
+                $scope.message = 'Username or Password is Wrong';
+                $location.path('/');
+            }    
                 
 		 }).error(function(data,status,headers,config) 
          {
-                console.log("error occured")
-		 	    $location.path('/')
+                console.log("error occured");
+                $scope.message = 'Server is down. Please try again after some time';
+		 	    $location.path('/');
         });
     }
+    
+    $scope.navbar = function()
+    {
+        var role = $rootScope.role;
+        if(role == 'TrainingExecutive')
+            return 'adminbar.html';
+        else if(role == 'Trainer')
+            return 'trainerbar.html';
+        else if(role == 'Employee')
+            return 'userbar.html';
+        else
+            return '';
+    }
+    
+    $scope.register = function()
+    {
+        console.log($location);
+        $location.path('/dashboard');
+    }
+   
+    
 });
+
+ myApp.controller('registerTraining', function($rootScope,$scope,$http,$location)
+ {
+     $scope.postData = function()
+     {
+        var training = $scope.training;
+        console.log(training); 
+     
+     
+       $http(
+				{   method:'POST',
+					url:"http://localhost:8080/springmvchibernate/registertraining",
+					data: training,
+					headers: {'Content-Type' : 'application/json'}
+				}
+			).success(function(data,status,headers,config) 
+				{
+                    console.log(data.name);
+                
+		 }).error(function(data,status,headers,config) 
+         {
+                console.log("error occured");
+                $scope.message = 'Server is down. Please try again after some time';
+		 	    $location.path('/');
+        });
+     }
+ });
